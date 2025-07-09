2cf26eb5fc7195dd46b2dfbd0c8ae41772a6283b
Ammar Wahidi <aammarahmed123@gmail.com>
2025-07-09 22:05:50 +0300

    CA

---
+++ Floating Point/IEEE 754 Addition/RTL/IEEE754pack.v
@@ -0,0 +1,10 @@
+module IEEE754pack(
+    input sign ,
+    input  [7:0] Exponent,
+    input [23:0] Fraction_withhidden_1,
+    output  [31:0] x 
+);
+
+assign x = {sign , Exponent , Fraction_withhidden_1[22:0] };
+
+endmodule
